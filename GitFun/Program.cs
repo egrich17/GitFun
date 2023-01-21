582bcd18b8ca2af6333452fbf7fc686cae572dba
Ellie Richardson <egr27@byu.edu>
2023-01-21 16:35:23 -0700

    Modified console line

--- GitFun/Program.cs
+++ GitFun/Program.cs
@@ -6,7 +6,7 @@ namespace GitFun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I made a change!");
         }
     }
 }
